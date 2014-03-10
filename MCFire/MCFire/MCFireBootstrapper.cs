9b1e6bce63db089bd9406c1a8c777867288a7aaf
Tristyn <expiredrice@live.ca>
2014-03-09 22:10:59 -0700

    Added modding support.

--- MCFire/MCFire/MCFireBootstrapper.cs
+++ MCFire/MCFire/MCFireBootstrapper.cs
@@ -1,4 +1,8 @@
-﻿using Gemini;
+﻿using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.ReflectionModel;
+using System.IO;
+using System.Security;
+using Gemini;
 using Gemini.Framework;
 using System;
 using System.Collections.Generic;
@@ -12,14 +16,65 @@ namespace MCFire
 {
     class MCFireBootstrapper : AppBootstrapper
     {
-        protected override IEnumerable<System.Reflection.Assembly> SelectAssemblies()
+        protected override IEnumerable<Assembly> SelectAssemblies()
         {
-            var list = new List<Assembly>(base.SelectAssemblies())
+            // get the application assembly, and the Metro assembly.
+            var assemblies = new List<Assembly>(base.SelectAssemblies())
             {
-                typeof(Gemini.Modules.Metro.ViewModels.MainWindowViewModel).Assembly,
-                typeof(MCFire.Modules.HelloWorld.HelloWorld.ViewModels.HelloWorldViewModel).Assembly
+                typeof (Gemini.Modules.Metro.ViewModels.MainWindowViewModel).Assembly
             };
-            return list;
+
+            // get assemblies in mods folder
+            try
+            {
+                var modsPath = Path.GetFullPath(@"./.MODS/");
+                //create mods folder if it doesnt exist, then walk its subdirectories
+                Directory.CreateDirectory(modsPath);
+                foreach (var directoryInfo in WalkDirectoryTree(new DirectoryInfo(modsPath)))
+                {
+                    var catalog = new DirectoryCatalog(directoryInfo.FullName);
+
+                    // hack out the mods assemblies with this horrible code.
+                    assemblies.AddRange(catalog.Parts
+                        .Select(part => ReflectionModelServices.GetPartType(part).Value.Assembly)
+                        .Where(assembly => !assemblies.Contains(assembly)));
+                }
+            }
+            catch (SecurityException) { Console.WriteLine("Could not create mods folder. Security exception."); }
+            catch (PathTooLongException) { Console.WriteLine("Could not create mods folder. Path too long."); }
+            catch (UnauthorizedAccessException) { Console.WriteLine("Could not create mods folder. Unauthorized access."); }
+            catch (DirectoryNotFoundException) { Console.WriteLine("Could not create mods folder. Directory unavailable."); }
+
+            // reverse assemblies, so mods are prioritized.
+            assemblies.Reverse();
+            return assemblies;
+        }
+
+        /// <summary>
+        /// returns all subdirectories in the root directory.
+        /// root directory is included.
+        /// </summary>
+        /// <param name="root">The directory to begin the search.</param>
+        private static IEnumerable<DirectoryInfo> WalkDirectoryTree(DirectoryInfo root)
+        {
+            var directories = new List<DirectoryInfo> { root };
+
+            try
+            {
+                var subDirs = root.GetDirectories();
+
+                foreach (var dirInfo in subDirs)
+                {
+                    // yes i know this is quadratic. it works
+
+                    directories.AddRange(WalkDirectoryTree(dirInfo));
+                }
+            }
+            catch (DirectoryNotFoundException) { }
+            catch (SecurityException) { }
+            catch (UnauthorizedAccessException) { }
+
+            return directories;
         }
     }
 }
