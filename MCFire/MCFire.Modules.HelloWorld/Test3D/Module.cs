8ac12701cbfd3587b0f80ba43569c431e34381b9
Tristyn <expiredrice@live.ca>
2014-03-26 07:42:47 -0700

    Add 3D test

---
+++ MCFire/MCFire.Modules.HelloWorld/Test3D/Module.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Caliburn.Micro;
+using Gemini.Framework;
+using Gemini.Framework.Results;
+using Gemini.Modules.MainMenu.Models;
+using MCFire.Modules.HelloWorld.Test3D.ViewModels;
+
+namespace MCFire.Modules.HelloWorld.Test3D
+{
+    [Export(typeof(IModule))]
+    public class Module : ModuleBase
+    {
+        public override void Initialize()
+        {
+            MainMenu.All.First(menu => menu.Name=="View")
+                .Add(new MenuItem("3D Test", Open3DTest));
+        }
+
+        static IEnumerable<IResult> Open3DTest()
+        {
+            yield return Show.Document<D3DViewModel>();
+        }
+    }
+}
---
+++ MCFire/MCFire.Modules.HelloWorld/Test3D/ViewModels/3DViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Composition;
+using Gemini.Framework;
+using MCFire.Modules.Infrastructure.D3D.Controls;
+using SharpDX;
+using SharpDX.Toolkit.Graphics;
+
+namespace MCFire.Modules.HelloWorld.Test3D.ViewModels
+{
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [Export]
+    public class D3DViewModel : Document
+    {
+        GeometricPrimitive _primitive;
+        BasicEffect _basicEffect;
+
+        public void OnLoadContent(object sender, GraphicsDeviceEventArgs e)
+        {
+            _primitive = GeometricPrimitive.Teapot.New(e.GraphicsDevice);
+            _basicEffect = new BasicEffect(e.GraphicsDevice)
+            {
+                View = Matrix.LookAtLH(new Vector3(0, 0, 3), Vector3.Zero, Vector3.UnitY),
+                World = Matrix.Identity,
+                PreferPerPixelLighting = true,
+            };
+            _basicEffect.EnableDefaultLighting();
+            _basicEffect.Texture = Texture2D.Load(e.GraphicsDevice, "Test3D/Resources/Fabric.png");
+            _basicEffect.TextureEnabled = true;
+        }
+    }
+}
