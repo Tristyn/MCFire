9d5047f0bb520c978df7115141948c088a992506
Tristyn <expiredrice@live.ca>
2014-03-27 07:48:31 -0700

    D3DView was excluded. Why you do this to me git

---
+++ MCFire/MCFire.Modules.HelloWorld/Test3D/Views/D3DView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MCFire.Modules.HelloWorld.Test3D.Views
+{
+    /// <summary>
+    /// Interaction logic for _3DView.xaml
+    /// </summary>
+    public partial class D3DView
+    {
+        public D3DView()
+        {
+            InitializeComponent();
+        }
+    }
+}
