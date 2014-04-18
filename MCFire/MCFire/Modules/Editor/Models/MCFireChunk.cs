8dba264c2366eb10a9abbed7413c7bcd466fa556
Tristyn <expiredrice@live.ca>
2014-04-18 03:54:02 -0700

    files not tracked in git... oops STAAACK

---
+++ MCFire/MCFire/Modules/Editor/Models/MCFireChunk.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Substrate;
+
+namespace MCFire.Modules.Editor.Models
+{
+    /// <summary>
+    /// A representation of a Minecraft chunk for use by the editor.
+    /// </summary>
+    public class MCFireChunk
+    {
+        public MCFireChunk(ChunkRef chunk)
+        {
+            SubstrateChunk = chunk;
+        }
+
+        [NotNull]
+        public ChunkRef SubstrateChunk { get; set; }
+
+        [CanBeNull]
+        public VisualChunk Visual { get; set; }
+    }
+}
